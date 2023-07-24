dc698d732786a0786382ecf4946d164c5183533c
Christopher Peret <cperet89@gmail.com>
2023-07-23 19:28:11 -0700

    TOS_V4

--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -18,13 +18,21 @@ export function Register() {
 
     const handleSubmit = async (event) => {
         event.preventDefault();
+        
+        // check that all fields are filled out
+        if (!username || !email || !dateOfBirth || !password || !confirmPassword) {
+            console.error("All fields must be filled out");
+            return;
+        }
+    
+        // check that passwords match
         if (password !== confirmPassword) {
-            // Handle password mismatch
             console.error("Passwords do not match");
             return;
         }
         setTosModalOpen(true);
     };
+    
 
     const handleTosAccept = async () => {
         try {
